53d421786cb66b7400232fada03444cfc92c1a20
getjiggy <74795898+getjiggy@users.noreply.github.com>
2021-12-19 14:05:16 -0700

    hacky try except removed

--- PyBank/.ipynb_checkpoints/main-checkpoint.py
+++ PyBank/.ipynb_checkpoints/main-checkpoint.py
@@ -1,8 +1,12 @@
+#
+
+
 import pandas as pd
 
 df = pd.read_csv("C:\\Users\\mattr\\Class\\python_homework\\PyBank\\budget_data.csv")
 
-total_number_months = 0
+total_number_months = df['Date'].count()
+
 net_profit = df['Profit/Losses'].sum()
 change = df['Profit/Losses'][1] - df['Profit/Losses'][0]
 avg_change = 0
@@ -10,9 +14,10 @@ max_change = 0
 max_row = df['Date'][1]
 min_row = df['Date'][1]
 min_change = 0
+
 for index, row in df.iterrows():
-    total_number_months += 1
-    try: 
+    
+    if index != total_number_months - 1:
         avg_change += df['Profit/Losses'][index + 1] - df['Profit/Losses'][index]
         if max_change == 0:
             max_change = df['Profit/Losses'][index + 1] - df['Profit/Losses'][index]
@@ -26,8 +31,8 @@ for index, row in df.iterrows():
         elif min_change > df['Profit/Losses'][index + 1] - df['Profit/Losses'][index]:
             min_change = df['Profit/Losses'][index + 1] - df['Profit/Losses'][index]
             min_row = df['Date'][index + 1]
-    except:
-        continue
+    else:
+        break
 avg_change /= total_number_months - 1
 
 print('Financial Analysis')
@@ -37,7 +42,3 @@ print(f'Total Profit: ${round(net_profit)}')
 print(f'    Average Change: ${round(avg_change, 2)}')
 print(f'Greatest Increase in Profits: {max_row} (${max_change})')
 print(f'Greatest Decrease in Profits: {min_row} (${min_change})')
-
-
-
-
