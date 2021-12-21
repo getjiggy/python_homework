1fc87469de22141df5d08228b34869b3dffb9332
getjiggy <74795898+getjiggy@users.noreply.github.com>
2021-12-20 17:54:52 -0700

    added comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,20 +1,23 @@
-#
-
-
 import pandas as pd
 
 df = pd.read_csv("C:\\Users\\mattr\\Class\\python_homework\\PyBank\\budget_data.csv")
 
+# getting total number of months
 total_number_months = df['Date'].count()
 
+# summing profits/losses
 net_profit = df['Profit/Losses'].sum()
-change = df['Profit/Losses'][1] - df['Profit/Losses'][0]
+
+# setting variables for iteration
 avg_change = 0
 max_change = 0
-max_row = df['Date'][1]
-min_row = df['Date'][1]
 min_change = 0
 
+# setting max_row and min_row to first record for iteration
+max_row = df['Date'][1]
+min_row = df['Date'][1]
+
+# iterating over df to calculate avg_change, max_change, min_change, max_row, min_row
 for i in df.index:
     if i != total_number_months - 1:
         avg_change += df['Profit/Losses'][i + 1] - df['Profit/Losses'][i]
@@ -32,8 +35,10 @@ for i in df.index:
             min_row = df['Date'][i + 1]
     else:
         break
+# finalizing avg_change        
 avg_change /= total_number_months - 1
 
+# printing results
 print('Financial Analysis')
 print('-----------------------')
 print(f'Total Months: {total_number_months}')
